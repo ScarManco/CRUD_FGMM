956bf8edbfefa1f8d1ada02010ee289a92e309df
ScarManco <135183926+ScarManco@users.noreply.github.com>
2023-12-07 23:27:58 -0600

    ae.php

--- crud_FGMM/ae.php
+++ crud_FGMM/ae.php
@@ -13,17 +13,17 @@ if ($conn->connect_error) {
   die("Conexion fallida: " . $conn->connect_error);
 }
 $nombre = isset($_POST['nombre']) ? $_POST['nombre'] : "";
-$telefono = isset($_POST['te$telefono']) ? $_POST['te$telefono'] : "";
-$direccion = isset($_POST['dire$direccion']) ? $_POST['dire$direccion'] : "";
+$telefono = isset($_POST['telefono']) ? $_POST['telefono'] : "";
+$direccion = isset($_POST['direccion']) ? $_POST['direccion'] : "";
 
 
-$sql = "INSERT INTO frutas (nombre, te$telefono, dire$direccion ) VALUES ('$nombre', '$telefono', '$direccion')";
+$sql = "INSERT INTO empleados (nombre, telefono, direccion ) VALUES ('$nombre', '$telefono', '$direccion')";
 
 if($_POST){
     echo "<script lenguage='JavaScript'>
     alert(''Los datos son correctos);
     ";
-    header("Location: me.php"); // Redirigir a la página de inicio después de iniciar sesión
+    header("Location: consultae.php"); // Redirigir a la página de inicio después de iniciar sesión
 }else{
     echo "<script lenguage='JavaScript'>
     alert(''Los datos son incorrectos);
