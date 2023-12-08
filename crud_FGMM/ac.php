9b980dc22f50697e1e2083ec9cf85bb4686d916a
ScarManco <135183926+ScarManco@users.noreply.github.com>
2023-12-07 23:29:12 -0600

    ac.php

--- crud_FGMM/ac.php
+++ crud_FGMM/ac.php
@@ -13,11 +13,11 @@ if ($conn->connect_error) {
   die("Conexion fallida: " . $conn->connect_error);
 }
 $nombre = isset($_POST['nombre']) ? $_POST['nombre'] : "";
-$telefono = isset($_POST['te$telefono']) ? $_POST['te$telefono'] : "";
-$direccion = isset($_POST['dire$direccion']) ? $_POST['dire$direccion'] : "";
+$telefono = isset($_POST['telefono']) ? $_POST['telefono'] : "";
+$direccion = isset($_POST['direccion']) ? $_POST['direccion'] : "";
 
 
-$sql = "INSERT INTO clientes (nombre, te$telefono, dire$direccion ) VALUES ('$nombre', '$telefono', '$direccion')";
+$sql = "INSERT INTO clientes (nombre, telefono, direccion ) VALUES ('$nombre', '$telefono', '$direccion')";
 
 if($_POST){
     echo "<script lenguage='JavaScript'>
